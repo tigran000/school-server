08a89755a3c574827e02f693b43a75bb7ac89bab
Tigran <tigranharutynyan59@gmailcom>
2019-02-28 15:55:07 +0400

    FINAL version

--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const db = require('../db')
+const db = require('../db');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    const { credentials } = req.body
-    const sql = ` SELECT * FROM users WHERE userName = '${credentials.userName}'`
+    const { credentials } = req.body;
+    const sql = ` SELECT * FROM users WHERE userName = '${credentials.userName}'`;
     db.query(sql, (err, result) => {
         if (err) throw err;
-        const user = result[0]
+        const user = result[0];
         if (user) {
             bcrypt
                 .compare(credentials.password, user.password)
                 .then(doMatch => {
                     if (doMatch) {
-                        delete user.password
+                        delete user.password;
                         jwt.sign({ user }, 'tigranssecretkey', (err, token) => {
                             res.json({
                                 user,
@@ -23,9 +23,9 @@ router.post('/login', (req, res) => {
                             });
                         });
                     } else res.status(401).send('UNAUTHORIZED');
-                })
+                });
         } else res.status(401).send('UNAUTHORIZED');
-    })
+    });
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/profile', isAuth, (req, res) => {
-    const { user } = req.token
+    const { user } = req.token;
     // will add sql request if needed
     res.json({ user });
 });
