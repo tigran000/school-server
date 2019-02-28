9a02a6331399f1a510ca7ce12e62d4290e4d4f21
Tigran <tigranharutynyan59@gmailcom>
2019-02-28 12:30:22 +0400

    change structure

--- server.js
+++ server.js
@@ -1,58 +1,14 @@
 const express = require('express');
-const mysql = require('mysql')
-const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const isAuth = require('./is-auth');
+const cors = require('cors');
+const loginRoute = require('./routes/login');
+const profileRoute = require('./routes/profile');
 const app = express();
 app.use(bodyParser.json());
 
-
-var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'emerson59',
-    database: 'testdb'
-})
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySql Connected...');
-});
-
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-app.post('/login', (req, res) => {
-    const { credentials } = req.body
-    const sql = ` SELECT userName, isAdmin  FROM users WHERE userName = '${credentials.userName}' AND password = '${credentials.password}' `
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        const user = result[0]
-        console.log(user)
-         user ? user.isAdmin = Boolean(user.isAdmin) : undefined
-        jwt.sign({ user }, 'tigranssecretkey', (err, token) => {
-            res.json({
-                user,
-                token
-            });
-        });
-    })
-});
-
-app.get('/profile', isAuth, (req, res) => {
-    const { user } = req.token
-    // will add sql request at later
-    res.json({ user })
-});
-
-
+app.use(cors());
+app.use(loginRoute);
+app.use(profileRoute);
 
 app.listen(8000, () => {
     console.log('Server started on port 8000');
